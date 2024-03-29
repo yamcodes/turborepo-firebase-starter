8ca2e4c827b32251c2886cfb2d3e21032fc1c7a5
Yam Borodetsky <hi@yam.codes>
2024-03-29 20:02:47 +0500

    fix: broken emulation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,36 @@
-import { iLikeTurtles } from "@acme/shared";
-import { onRequest } from "firebase-functions/v2/https";
-import { faker } from "@faker-js/faker";
-import express from "express";
+// /**
+//  * Import function triggers from their respective submodules:
+//  *
+//  * import {onCall} from "firebase-functions/v2/https";
+//  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+//  *
+//  * See a full list of supported triggers at https://firebase.google.com/docs/functions
+//  */
 
-const app = express();
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
-app.get("*", (_req, res) => {
-  res.send({
-    hello: "world",
-    randomName: faker.person.firstName(),
-    iLikeTurtles: iLikeTurtles(),
-    nice: 69,
-  });
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+
+// // export const helloWorld = onRequest((request, response) => {
+// //   logger.info("Hello logs!", {structuredData: true});
+// //   response.send("Hello from Firebase!");
+// // });
+
+import { onRequest } from 'firebase-functions/v2/https';
+import Fastify, { FastifyPluginAsync } from 'fastify';
+import { registerRoutes } from './utils';
+
+const fastify = Fastify({
+  logger: true,
 });
 
+const app: FastifyPluginAsync = async (request, reply) => {
+  await registerRoutes(fastify);
+  await fastify.ready();
+  fastify.server.emit('request', request, reply);
+};
+
+// @ts-ignore -- firebase functions typescript is set to only support express, but it actually supports fastify
 export const server = onRequest(app);
---
+++ apps/api/src/utils.ts
@@ -0,0 +1,39 @@
+import { iLikeTurtles } from '@acme/shared';
+import { faker } from '@faker-js/faker';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+
+export interface Request extends FastifyRequest {
+  /** The wire format representation of the request body. */
+  rawBody: Buffer;
+}
+
+export const registerRoutes = async (fastify: FastifyInstance) => {
+  fastify.addContentTypeParser('application/json', {}, (req, payload, done) => {
+    // useful to include the request's raw body on the `req` object that will
+    // later be available in your other routes so you can calculate the HMAC
+    // if needed
+    // @ts-ignore - taken from Fastify docs
+    req.rawBody = payload.rawBody;
+
+    // payload.body is already the parsed JSON so we just fire the done callback
+    // with it
+    // @ts-ignore - taken from Fastify docs
+    done(null, payload.body);
+  });
+
+  // define your endpoints here...
+  fastify.post('/some-route-here', async (request, reply) => {});
+
+  fastify.get('/', async (request, reply) => {
+    reply.send({ message: 'Hello World!!' });
+  });
+
+  fastify.get('/legacy', (_req, res) => {
+    res.send({
+      hello: 'world',
+      randomName: faker.person.firstName(),
+      iLikeTurtles: iLikeTurtles(),
+      nice: 69,
+    });
+  });
+};
