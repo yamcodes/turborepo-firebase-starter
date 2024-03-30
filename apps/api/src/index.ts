97508c1e4eeb0547b2d101ead0193eec4c523802
Yam Borodetsky <hi@yam.codes>
2024-03-30 19:46:40 +0500

    fix(api): switch back to us-central1
    It's faster for some reason

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -45,12 +45,7 @@ setContentTypeParser(
 
 registerRoutes(app);
 
-export const server = onRequest(
-  {
-    region: 'asia-south1',
-  },
-  async (request, reply) => {
-    await app.ready();
-    app.server.emit('request', request, reply);
-  }
-);
+export const server = onRequest(async (request, reply) => {
+  await app.ready();
+  app.server.emit('request', request, reply);
+});
